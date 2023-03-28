db2348d3ce03baf549614894622277fe0e31edbf
miriancp88 <117675852+miriancp88@users.noreply.github.com>
2023-03-28 15:54:37 +0200

    Test velocidad no menor a 0

--- tests/TestCoche.java
+++ tests/TestCoche.java
@@ -5,15 +5,32 @@ import java.util.function.Supplier;
 
 public class TestCoche {
     @Test
-    public void test_al_crear_un_coche_su_velocidad_es_cero(){
+    public void test_al_crear_un_coche_su_velocidad_es_cero() {
         Coche nuevoCoche = new Coche();
         Assertions.assertEquals(0, nuevoCoche.velocidad);
     }
 
     @Test
-    public void test_al_acelerar_un_coche_su_velocidad_aumenta(){
+    public void test_al_acelerar_un_coche_su_velocidad_aumenta() {
         Coche nuevoCoche = new Coche();
         nuevoCoche.acelerar(30);
         Assertions.assertEquals(30, nuevoCoche.velocidad);
     }
+
+    @Test
+    public void test_al_descelerar_un_coche_su_velocidad_disminuye() {
+        Coche nuevoCoche = new Coche();
+        nuevoCoche.velocidad = 50;
+        nuevoCoche.decelerar(20);
+        Assertions.assertEquals(30, nuevoCoche.velocidad);
+    }
+
+    @Test
+    public void test_al_acelerar_un_coche_su_velocidad_no_puede_ser_mernor_que_cero() {
+        Coche nuevoCoche = new Coche();
+        nuevoCoche.velocidad = 50;
+        nuevoCoche.decelerar(80);
+        Assertions.assertEquals(0, nuevoCoche.velocidad);
+    }
+
 }
