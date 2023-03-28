a4c305fba000a72561c8a3a6579c4ebbb9a56689
miriancp88 <117675852+miriancp88@users.noreply.github.com>
2023-03-28 15:37:40 +0200

    Test acelerar

--- tests/TestCoche.java
+++ tests/TestCoche.java
@@ -6,10 +6,14 @@ import java.util.function.Supplier;
 public class TestCoche {
     @Test
     public void test_al_crear_un_coche_su_velocidad_es_cero(){
-
         Coche nuevoCoche = new Coche();
-
-
         Assertions.assertEquals(0, nuevoCoche.velocidad);
+    }
+
+    @Test
+    public void test_al_acelerar_un_coche_su_velocidad_aumenta(){
+        Coche nuevoCoche = new Coche();
+        nuevoCoche.acelerar(30);
+        Assertions.assertEquals(30, nuevoCoche.velocidad);
     }
 }
