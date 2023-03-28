30823ec4788a01efc97f0b21b75655780f169f45
miriancp88 <117675852+miriancp88@users.noreply.github.com>
2023-03-28 15:58:31 +0200

    mi nombre en todos los metodos y test

--- tests/Coche.java
+++ tests/Coche.java
@@ -1,11 +1,11 @@
 public class Coche {
     public int velocidad;
 
-    public void acelerar(int aceleracion) {
+    public void acelerarMIRIAN(int aceleracion) {
         velocidad += aceleracion;
     }
 
-    public void decelerar(int deceleracion) {
+    public void decelerarMIRIAN(int deceleracion) {
         velocidad -= deceleracion;
         if (velocidad<0) velocidad = 0;
     }
--- tests/TestCoche.java
+++ tests/TestCoche.java
@@ -1,35 +1,33 @@
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
-import java.util.function.Supplier;
-
 public class TestCoche {
     @Test
-    public void test_al_crear_un_coche_su_velocidad_es_cero() {
+    public void test_al_crear_un_coche_su_velocidad_es_cero_MIRIAN() {
         Coche nuevoCoche = new Coche();
         Assertions.assertEquals(0, nuevoCoche.velocidad);
     }
 
     @Test
-    public void test_al_acelerar_un_coche_su_velocidad_aumenta() {
+    public void test_al_acelerar_un_coche_su_velocidad_aumenta_MIRIAN() {
         Coche nuevoCoche = new Coche();
-        nuevoCoche.acelerar(30);
+        nuevoCoche.acelerarMIRIAN(30);
         Assertions.assertEquals(30, nuevoCoche.velocidad);
     }
 
     @Test
-    public void test_al_descelerar_un_coche_su_velocidad_disminuye() {
+    public void test_al_descelerar_un_coche_su_velocidad_disminuye_MIRIAN() {
         Coche nuevoCoche = new Coche();
         nuevoCoche.velocidad = 50;
-        nuevoCoche.decelerar(20);
+        nuevoCoche.decelerarMIRIAN(20);
         Assertions.assertEquals(30, nuevoCoche.velocidad);
     }
 
     @Test
-    public void test_al_acelerar_un_coche_su_velocidad_no_puede_ser_mernor_que_cero() {
+    public void test_al_acelerar_un_coche_su_velocidad_no_puede_ser_mernor_que_cero_MIRIAN() {
         Coche nuevoCoche = new Coche();
         nuevoCoche.velocidad = 50;
-        nuevoCoche.decelerar(80);
+        nuevoCoche.decelerarMIRIAN(80);
         Assertions.assertEquals(0, nuevoCoche.velocidad);
     }
 
